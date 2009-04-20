a784569a0e3727f31bf0112f55d1bef027343ef6
Yehuda Katz <wycats@gmail.com>
2009-04-20 13:54:42 -0700

    Adds a test case

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -16,6 +16,31 @@ module GemResolver
         "bar" => ["2.0.0"]
       )
     end
+    
+    it "supports a crazy case" do
+      index = build_index do
+        add_spec "activemerchant", "1.4.1" do
+          runtime "activesupport", ">= 1.4.1"
+        end
+        add_spec "activesupport", "3.0.0"
+        add_spec "activesupport", "2.3.2"
+        add_spec "action_pack", "2.3.2" do
+          runtime "activesupport", "= 2.3.2"
+        end
+      end
+      
+      deps = [
+        build_dep("activemechant", ">= 0"),
+        build_dep("action_pack", "= 2.3.2")
+      ]
+      
+      specs = GemResolver.resolve(deps, index)
+      specs.should match_gems(
+        "activemerchant" => "1.4.1",
+        "action_pack" => "2.3.2",
+        "activesupport" => "2.3.2"
+      )
+    end
 
     it "supports nested dependencies" do
       index = build_index do
